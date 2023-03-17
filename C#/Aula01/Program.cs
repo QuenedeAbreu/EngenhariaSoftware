14aea3e6a2735d85dd2716971efc6704863783f7
Quenede Abreu <quenede.in@gmail.com>
2023-03-17 18:34:48 -0500

    First push

---
+++ C#/Aula01/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+public class Program
+{
+  public static void Main()
+  {
+    int numero = 10;
+
+    if (numero == 10)
+    {
+      int retorn = alterar(numero);
+    }
+    else { }
+    System.Console.WriteLine(numero);
+
+    int alterar(int a)
+    {
+      return a + 1;
+    }
+
+    string[] nomes = new string[] { "oi", "oi2", "oi3" };
+    foreach (string nome in nomes)
+    {
+      System.Console.WriteLine(nome);
+      break;
+    }
+  }
+
+}
